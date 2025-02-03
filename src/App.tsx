25ed4688b8787a3f4d2ab4a1aa8353a0743cfd6a
xellosiris <xellosiris@gmail.com>
2025-02-03 17:08:07 +0800

    use shadcn UI and TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Subtitle } from "./Subtitle";
+import { Toaster } from "./components/ui/toaster";
+import "./index.css";
+function App() {
+  return (
+    <>
+      <Toaster />
+      <Subtitle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Subtitle.tsx
@@ -0,0 +1,88 @@
+import axios from "axios";
+import { Send } from "lucide-react";
+import { useState } from "react";
+import { WebVTTParser } from "webvtt-parser";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import { Textarea } from "./components/ui/textarea";
+import { useToast } from "./hooks/use-toast";
+
+const parser = new WebVTTParser();
+
+export function Subtitle() {
+  const [url, setURL] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [title, setTitle] = useState<string | undefined>(undefined);
+  const { toast } = useToast();
+  const change = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setURL(event.target.value);
+  };
+
+  const submit = async () => {
+    try {
+      const urlParams = new URL(url).searchParams;
+      const lang = urlParams.get("wtlocale");
+      const lank = urlParams.get("lank");
+      const itemInfoUrl = `https://b.jw-cdn.org/apis/mediator/v1/media-items/${lang}/${lank}`;
+      const payload = await axios.get(itemInfoUrl);
+      const subtitleURL = payload.data.media[0].files[0].subtitles.url;
+      const res = await axios.get(subtitleURL);
+      const subtitles = parser.parse(res.data, "subtitles");
+      const subs = subtitles.cues.map((s) => s.text);
+      setSubtitle(subs.join("\n"));
+      setTitle(payload.data.media[0].title);
+    } catch (e) {
+      toast({
+        title: "錯誤",
+        description: "請檢查網址是否正確",
+        variant: "destructive",
+      });
+    }
+  };
+  return (
+    <div className="flex flex-col h-screen max-w-2xl p-3 mx-auto space-y-3 text-sm text-foreground">
+      <h1 className="text-2xl">JW Subtitle Extract</h1>
+      <div>
+        <Label>使用方式</Label>
+        <div className="text-muted-foreground">
+          請複製影片的分享連結並貼在下方欄位，
+          <span className="text-destructive">請不要從瀏覽器網址複製</span>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <Input
+          className="w-full"
+          placeholder="請把影片連結貼在這裡"
+          value={url}
+          onChange={change}
+        />
+        <Button onClick={submit}>送出</Button>
+        <Button
+          variant={"destructive"}
+          onClick={() => {
+            setSubtitle("");
+            setURL("");
+            setTitle(undefined);
+          }}
+        >
+          清除
+        </Button>
+      </div>
+      {!!title && (
+        <div className="flex flex-col gap-1.5">
+          <Label>影片名稱</Label>
+          <p>{title}</p>
+        </div>
+      )}
+      <div className="h-full flex flex-col gap-1.5">
+        <Label>字幕內容</Label>
+        <Textarea value={subtitle} className="h-full" />
+      </div>
+      <div className="flex items-center space-x-3 text-sm text-muted-foreground">
+        <Send size={16} />
+        <span>xellosiris@gmail.com</span>
+      </div>
+    </div>
+  );
+}
